c3fbbeac4d80ae0f5bfe17686c57b4cceaec3293
Consuk <0228053@up.edu.mx>
2025-08-17 19:49:41 -0600

    Resnet encoders version modifications

--- EndoSLAM-master/EndoSfMLearner/models/resnet_encoder.py
+++ EndoSLAM-master/EndoSfMLearner/models/resnet_encoder.py
@@ -1,69 +1,72 @@
-# Copyright Niantic 2019. Patent Pending. All rights reserved.
-#
-# This software is licensed under the terms of the Monodepth2 licence
-# which allows for non-commercial use only, the full terms of which are made
-# available in the LICENSE file.
-
-from __future__ import absolute_import, division, print_function
-
-import numpy as np
-
+# models/resnet_encoder.py
 import torch
 import torch.nn as nn
-import torchvision.models as models
-import torch.utils.model_zoo as model_zoo
+from torchvision.models import resnet18, resnet34, resnet50
+from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights
+import numpy as np
 
+"""
+Compatibilidad con torchvision >= 0.13:
+- Usamos enums de pesos (ResNet18_Weights, etc.) en lugar de model_urls/pretrained=.
+- Para entrada multi-imagen (p.ej., PoseNet con 2 imágenes), expandimos conv1 de 3->3*num_imgs
+  y la inicializamos copiando pesos preentrenados (promedio por canal y réplica).
+"""
 
-class ResNetMultiImageInput(models.ResNet):
-    """Constructs a resnet model with varying number of input images.
-    Adapted from https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+def _get_weights_enum(num_layers, pretrained: bool):
+    if not pretrained:
+        return None
+    if num_layers == 18:
+        return ResNet18_Weights.IMAGENET1K_V1
+    elif num_layers == 34:
+        return ResNet34_Weights.IMAGENET1K_V1
+    elif num_layers == 50:
+        return ResNet50_Weights.IMAGENET1K_V1
+    else:
+        raise ValueError(f"Unsupported resnet layers: {num_layers}")
+
+def _build_resnet_backbone(num_layers, weights):
+    if num_layers == 18:
+        return resnet18(weights=weights)
+    elif num_layers == 34:
+        return resnet34(weights=weights)
+    elif num_layers == 50:
+        return resnet50(weights=weights)
+    else:
+        raise ValueError(f"Unsupported resnet layers: {num_layers}")
+
+def resnet_multiimage_input(num_layers=18, pretrained=True, num_input_images=2):
     """
-    def __init__(self, block, layers, num_classes=1000, num_input_images=1):
-        super(ResNetMultiImageInput, self).__init__(block, layers)
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(
-            num_input_images * 3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=False)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0.2)
-
-
-def resnet_multiimage_input(num_layers, pretrained=False, num_input_images=1):
-    """Constructs a ResNet model.
-    Args:
-        num_layers (int): Number of resnet layers. Must be 18 or 50
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        num_input_images (int): Number of frames stacked as input
+    Crea un ResNet preentrenado y adapta conv1 para num_input_images>1.
     """
-    assert num_layers in [18, 50], "Can only run with 18 or 50 layer resnet"
-    blocks = {18: [2, 2, 2, 2], 50: [3, 4, 6, 3]}[num_layers]
-    block_type = {18: models.resnet.BasicBlock, 50: models.resnet.Bottleneck}[num_layers]
-    model = ResNetMultiImageInput(block_type, blocks, num_input_images=num_input_images)
+    weights = _get_weights_enum(num_layers, pretrained)
+    resnet = _build_resnet_backbone(num_layers, weights)
 
-    if pretrained:
-        loaded = model_zoo.load_url(models.resnet.model_urls['resnet{}'.format(num_layers)])
-        loaded['conv1.weight'] = torch.cat(
-            [loaded['conv1.weight']] * num_input_images, 1) / num_input_images
-        model.load_state_dict(loaded)
-    return model
+    if num_input_images > 1:
+        old_conv = resnet.conv1                      # (out_c, 3, k, k)
+        in_channels = 3 * num_input_images
+        new_conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=old_conv.out_channels,
+            kernel_size=old_conv.kernel_size,
+            stride=old_conv.stride,
+            padding=old_conv.padding,
+            bias=False
+        )
+        with torch.no_grad():
+            # promedio en canales y réplica equilibrada
+            mean_w = old_conv.weight.mean(dim=1, keepdim=True)   # (out_c,1,k,k)
+            new_w = mean_w.repeat(1, in_channels, 1, 1) / float(num_input_images)
+            new_conv.weight.copy_(new_w)
+        resnet.conv1 = new_conv
+
+    return resnet
 
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=1):
         super(SpatialAttention, self).__init__()
         kernel_size = 1
         padding = 3 if kernel_size == 7 else 0
-        
+
         self.conv1 = nn.Conv2d(64, 4, kernel_size, padding=padding, bias=False)
         self.conv2 = nn.Conv2d(4, 4, kernel_size , padding=padding, bias=False)
         self.conv3 = nn.Conv2d(4, 64, kernel_size, padding=padding, bias=False)
@@ -73,66 +76,55 @@ class SpatialAttention(nn.Module):
         self.upsample = nn.Upsample(scale_factor=4)
 
     def forward(self, x):
-
         x1 = self.conv1(x)
         x2 = self.maxPooling(x1)
         reshaped1 = torch.reshape(x2,(x2.shape[0],x2.shape[1],-1,x2.shape[2]))
         y = torch.matmul(reshaped1,x2)
         z = self.relu(y)
         z = self.conv2(z)
-        t = self.softmax(z) 
+        t = self.softmax(z)
         out1 = torch.matmul(t,reshaped1)
         conv3_out = self.conv3(out1)
         upsample_out = self.upsample(conv3_out)
         k = torch.reshape(upsample_out,(upsample_out.shape[0],upsample_out.shape[1],-1,upsample_out.shape[2]))
-        output = k + x        
-                
+        output = k + x
         return output
 
-
 class ResnetEncoder(nn.Module):
-    """Pytorch module for a resnet encoder
-    """
-    def __init__(self, num_layers, pretrained, num_input_images=1):
+    """Encoder ResNet para Pose (multi-imagen)."""
+    def __init__(self, num_layers, pretrained, num_input_images=2):
         super(ResnetEncoder, self).__init__()
 
         self.num_ch_enc = np.array([64, 64, 128, 256, 512])
 
-        resnets = {18: models.resnet18,
-                   34: models.resnet34,
-                   50: models.resnet50,
-                   101: models.resnet101,
-                   152: models.resnet152}
+        if num_layers not in [18, 34, 50]:
+            raise ValueError(f"{num_layers} is not a valid number of resnet layers")
 
-        if num_layers not in resnets:
-            raise ValueError("{} is not a valid number of resnet layers".format(num_layers))
-
-        if num_input_images > 1:
-            self.encoder = resnet_multiimage_input(num_layers, pretrained, num_input_images)
-        else:
-            self.encoder = resnets[num_layers](pretrained)
+        self.encoder = resnet_multiimage_input(
+            num_layers=num_layers,
+            pretrained=pretrained,
+            num_input_images=num_input_images
+        )
 
         if num_layers > 34:
             self.num_ch_enc[1:] *= 4
-        
+
         self.SAB = SpatialAttention()
 
     def forward(self, input_image):
-        self.features = []
-        x = input_image
-        x = self.encoder.conv1(x)
+        # input_image: (B, 3*num_imgs, H, W)
+        features = []
+        x = self.encoder.conv1(input_image)
         x = self.encoder.bn1(x)
-        self.features.append(self.encoder.relu(x))
+        x = self.encoder.relu(x)
+        features.append(x)
 
-        #denemee
-        
-        self.features.append(self.SAB(self.features[-1]))
+        # atención espacial en la primera escala
+        features.append(self.SAB(features[-1]))
 
-
-        self.features.append(self.encoder.layer1(self.encoder.maxpool(self.features[-1])))
-        self.features.append(self.encoder.layer2(self.features[-1]))
-        self.features.append(self.encoder.layer3(self.features[-1]))
-        self.features.append(self.encoder.layer4(self.features[-1]))
-
-        return self.features
-
+        x = self.encoder.maxpool(features[-1])
+        x = self.encoder.layer1(x); features.append(x)
+        x = self.encoder.layer2(x); features.append(x)
+        x = self.encoder.layer3(x); features.append(x)
+        x = self.encoder.layer4(x); features.append(x)
+        return features
