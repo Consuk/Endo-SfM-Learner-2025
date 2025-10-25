6d76a9560acaec1a71d558d3276c335aa139c16c
Consuk <0228053@up.edu.mx>
2025-10-25 13:46:26 -0600

    train modifications

--- EndoSLAM-master/EndoSfMLearner/train.py
+++ EndoSLAM-master/EndoSfMLearner/train.py
@@ -11,6 +11,12 @@ import torch.utils.data
 import torchvision.utils as vutils
 import matplotlib.cm as cm
 
+import os, sys
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(CURRENT_DIR)
+if PARENT_DIR not in sys.path:
+    sys.path.insert(0, PARENT_DIR)
+
 import models
 import custom_transforms
 from utils import tensor2array, save_checkpoint
